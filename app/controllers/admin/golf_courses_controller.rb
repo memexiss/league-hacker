b4204be7fa01a695575f2196f740c0c473dbd654
Ben Eggett <beneggett@gmail.com>
2024-02-23 15:37:41 -0700

    Tweaks

--- app/controllers/admin/golf_courses_controller.rb
+++ app/controllers/admin/golf_courses_controller.rb
@@ -9,22 +9,19 @@ class Admin::GolfCoursesController < ApplicationController
   end
 
   def search
-    course_name = params[:course]
-    golf_course_api = GolfCourseApi.new
-    @course = golf_course_api.search(course_name)
-    @course_instance = GolfCourse.new(map_api_data(@course))
-    if !@course_instance.name.nil?
-      @course_instance.save
+    response = GolfCourseApi.new.search(params[:course])
+    @courses = process_response(response)
+    if @courses
       render partial: 'result'
-    else
-      redirect_to admin_golf_courses_path
+    else 
+      redirect_to admin_golf_courses_path, alert: "No courses found for #{params[:course]}"
     end
   end  
 
   def show 
     @course = GolfCourse.find(params[:id])
   end
-  
+
   def edit
     @course = GolfCourse.find(params[:id])
   end
@@ -72,69 +69,61 @@ class Admin::GolfCoursesController < ApplicationController
     )
   end
 
-  def map_api_data(api_data)
-    if api_data.present? && api_data[0].present?
-      coordinates = api_data[0]['coordinates']
-      course_attributes = {
-        remote_api_id: api_data[0]['_id'],
-        remote_api_version: api_data[0]['__v'],
-        address: api_data[0]['address'],
-        city: api_data[0]['city'],
-        latitude: coordinates.present? ? coordinates.split(',').first&.gsub('(', '').strip : nil,
-        longitude: coordinates.present? ? coordinates.split(',').last&.gsub(')', '').strip : nil,           
-        country: api_data[0]['country'],
-        fairway_grass: api_data[0]['fairwayGrass'],
-        green_grass: api_data[0]['greenGrass'],
-        number_of_holes: api_data[0]['holes'],
-        length_format: api_data[0]['lengthFormat'],
-        name: api_data[0]['name'],
-        phone: api_data[0]['phone'],
-        state: api_data[0]['state'],
-        website: api_data[0]['website'],
-        zip: api_data[0]['zip']
-      }
-      if api_data[0].dig("scorecard").present? 
-        holes_attributes = api_data[0].dig("scorecard").map do |data| 
-          hole_attributes = {hole: data["Hole"], par: data["Par"], handicap: data["Handicap"]}
-          if data["tees"].present? 
-            tees_attributes = data["tees"].map do |tee_data| 
-              name, t_data = tee_data
-              {name: name, color: t_data["color"], yards: t_data["yards"]}
-            end
-            hole_attributes[:tees_attributes] = tees_attributes
-          end
-          hole_attributes
-        end
-        course_attributes[:holes_attributes] = holes_attributes
-      end
-      course_attributes
-      if api_data[0].dig("teeBoxes").present? 
-        tee_box_attributes = api_data[0].dig("teeBoxes").map do |data| 
-          tee_box_attributes = {tee: data["tee"], slope: data["slope"], handicap: data["handicap"]}
-          tee_box_attributes
-        end
-        course_attributes[:tee_boxes_attributes] = tee_box_attributes
-      end
-      course_attributes
-    else
-      {
-        remote_api_id: nil,
-        address: nil,
-        city: nil,
-        latitude: nil,
-        longitude: nil,
-        country: nil,
-        fairway_grass: nil,
-        green_grass: nil,
-        number_of_holes: nil,
-        length_format: nil,
-        name: nil,
-        phone: nil,
-        state: nil,
-        website: nil,
-        zip: nil
-      }
+  def process_response(response)
+    return if response.is_a?(Hash) && response.dig('message') == 'No courses found' 
+
+    response.map do |course_data| 
+      remote_data = map_api_data(course_data)
+      course = GolfCourse.find_or_initialize_by remote_api_id: remote_data[:remote_api_id]
+      course.update(remote_data) if course.new_record?
+      course
     end
+  end
+  
+  def map_api_data(course_data)
+    coordinates = course_data['coordinates']
+    course_attributes = {
+      remote_api_id: course_data['_id'],
+      remote_api_version: course_data['__v'],
+      address: course_data['address'],
+      city: course_data['city'],
+      latitude: coordinates.present? ? coordinates.split(',').first&.gsub('(', '').strip : nil,
+      longitude: coordinates.present? ? coordinates.split(',').last&.gsub(')', '').strip : nil,           
+      country: course_data['country'],
+      fairway_grass: course_data['fairwayGrass'],
+      green_grass: course_data['greenGrass'],
+      number_of_holes: course_data['holes'],
+      length_format: course_data['lengthFormat'],
+      name: course_data['name'],
+      phone: course_data['phone'],
+      state: course_data['state'],
+      website: course_data['website'],
+      zip: course_data['zip']
+    }
+    if course_data.dig("scorecard").present? 
+      holes_attributes = course_data.dig("scorecard").map do |data| 
+        hole_attributes = {hole: data["Hole"], par: data["Par"], handicap: data["Handicap"]}
+        if data["tees"].present? 
+          tees_attributes = data["tees"].map do |tee_data| 
+            name, t_data = tee_data
+            {name: name, color: t_data["color"], yards: t_data["yards"]}
+          end
+          hole_attributes[:tees_attributes] = tees_attributes
+        end
+        hole_attributes
+      end
+      course_attributes[:holes_attributes] = holes_attributes
+    end
+    course_attributes
+    if course_data.dig("teeBoxes").present? 
+      tee_box_attributes = course_data.dig("teeBoxes").map do |data| 
+        tee_box_attributes = {tee: data["tee"], slope: data["slope"], handicap: data["handicap"]}
+        tee_box_attributes
+      end
+      course_attributes[:tee_boxes_attributes] = tee_box_attributes
+    end
+    course_attributes
+    
   end
   
 end
