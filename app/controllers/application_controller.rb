a4d199789d32a0b44e1e800e604e230b83e0c8f3
memexiss <adrianochuyt@hotmail.com>
2024-02-26 17:43:53 -0500

    added design to sign-up/log-in pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def verify_sign_in
     if !user_signed_in?
       redirect_to root_path
@@ -10,4 +12,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone])
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates_plausible_phone :phone, presence: true
   phony_normalize :phone, default_country_code: "US"
-  validates :name, presence: true
+  validates :password, :email, :name, presence: true
 
   enum role: {default: 0, admin: 10}
 end
