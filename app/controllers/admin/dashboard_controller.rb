1719aa0d36f6ed2b42cc023a897657f8f7118bf7
Ben Eggett <beneggett@gmail.com>
2024-03-01 17:02:35 -0700

    Dashboard/nav cleanup

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   before_action :verify_admin
 
   def index
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :golf_courses
   resources :leagues
   namespace :admin do
-    root 'leagues#index'
+    root 'dashboard#index'
     resources :golf_courses do
       collection do
         get 'search'
